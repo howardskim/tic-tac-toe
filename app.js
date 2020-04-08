dbf54e07bc3c8ece573097ef0f71c26136ea5d39
Howard Kim <howardkim820@gmail.com>
2020-04-08 11:57:03 -0700

    refactored

--- app.js
+++ app.js
@@ -19,15 +19,16 @@ let handleWin = (letter) => {
 }
 
 let checkStatus = () => {
-    let topLeft = gameCells[0].classList[2]
-    let topMiddle = gameCells[1].classList[2]
-    let topRight = gameCells[2].classList[2]
-    let middleLeft = gameCells[3].classList[2]
-    let middleMiddle = gameCells[4].classList[2]
-    let middleRight = gameCells[5].classList[2]
-    let bottomLeft = gameCells[6].classList[2]
-    let bottomMiddle = gameCells[7].classList[2]
-    let bottomRight = gameCells[8].classList[2]
+    console.log(gameCells[0].classList)
+    let topLeft = gameCells[0].classList[1]
+    let topMiddle = gameCells[1].classList[1]
+    let topRight = gameCells[2].classList[1]
+    let middleLeft = gameCells[3].classList[1]
+    let middleMiddle = gameCells[4].classList[1]
+    let middleRight = gameCells[5].classList[1]
+    let bottomLeft = gameCells[6].classList[1]
+    let bottomMiddle = gameCells[7].classList[1]
+    let bottomRight = gameCells[8].classList[1]
     let array = [topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight]
     if (xIsNext && !winner) {
         statusDiv.innerHTML = `${letterToSymbol('x')}'s turn`
@@ -36,20 +37,44 @@ let checkStatus = () => {
     }
     if(topLeft && topLeft === topMiddle && topLeft === topRight){
         handleWin(topLeft)
+        gameCells[0].classList.add('won')
+        gameCells[1].classList.add('won')
+        gameCells[2].classList.add('won')
     } else if(middleLeft && middleLeft === middleMiddle && middleLeft === middleRight){
         handleWin(middleLeft)
+        gameCells[3].classList.add('won')
+        gameCells[4].classList.add('won')
+        gameCells[5].classList.add('won')
     } else if(bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight){
         handleWin(bottomLeft)
+        gameCells[6].classList.add('won')
+        gameCells[7].classList.add('won')
+        gameCells[8].classList.add('won')
     } else if(topLeft && topLeft === middleLeft && topLeft === bottomLeft){
         handleWin(topLeft)
+        gameCells[0].classList.add('won')
+        gameCells[3].classList.add('won')
+        gameCells[6].classList.add('won')
     } else if(topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle){
         handleWin(topMiddle)
+        gameCells[1].classList.add('won')
+        gameCells[4].classList.add('won')
+        gameCells[7].classList.add('won')
     } else if(topRight && topRight === middleRight && topRight === bottomRight){
         handleWin(topRight);
+        gameCells[2].classList.add('won')
+        gameCells[5].classList.add('won')
+        gameCells[8].classList.add('won')
     } else if(topLeft && topLeft === middleMiddle && topLeft === bottomRight){
         handleWin(topLeft);
+        gameCells[0].classList.add('won')
+        gameCells[4].classList.add('won')
+        gameCells[8].classList.add('won')
     } else if(topRight && topRight === middleMiddle && topRight === bottomLeft){
         handleWin(topRight);
+        gameCells[2].classList.add('won')
+        gameCells[4].classList.add('won')
+        gameCells[6].classList.add('won')
     } else if(array.length === 9 && !array.includes(undefined)){
         liveGame = false;
         statusDiv.innerHTML = 'GAME IS A TIE'
@@ -69,6 +94,7 @@ let handleReset = (e) => {
     for(cell of gameCells){
         cell.classList.remove('x');
         cell.classList.remove('o');
+        cell.classList.remove('won')
     }
     statusDiv.innerHTML = `${letterToSymbol('x')}'s turn`
 
@@ -78,9 +104,10 @@ let handleReset = (e) => {
 let handleClick = (e) => {
     if(!liveGame) return;
     let classArray = getClass(e);
+    console.log(classArray)
     let location = classArray[1];
     let divElement = e.target;
-    if(classArray[2] === 'x' || classArray[2] === 'o') return;
+    if(classArray[1] === 'x' || classArray[1] === 'o') return;
     if(xIsNext){
         divElement.classList.add('x');;
         xIsNext = !xIsNext
